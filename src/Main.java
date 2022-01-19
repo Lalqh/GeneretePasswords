e5a06bd22fb05e941a1427b571e3d4603777a25f
lalqh <lalincotero13@gmail.com>
2022-01-18 21:31:58 -0600

    Functional main with two functions, it is missing that it only searches for a password.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import files.Files;
+import files.ReadFile;
+import files.WriteFile;
+import files.arguments.ObtainingData;
+import functions.GeneratePassoword;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -13,8 +19,9 @@ public class Main {
 
     private static FileOutputStream writeBin;
     private static FileInputStream readBin;
+    private static Scanner input = new Scanner(System.in);
 
-    public static void createDir(){
+    private static void createDir(){
         File directorry = new File("resources");
         if (directorry.mkdir()){
             initializeFiles();
@@ -23,33 +30,12 @@ public class Main {
         }
     }
 
-    public static void initializeFiles(){
+    private static void initializeFiles(){
         try {
             writeBin = new FileOutputStream("resources/password.bin", true);
             readBin = new FileInputStream("resources/password.bin");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-    }
-
-    public static void menu(){
-        Scanner input = new Scanner(System.in);
-        int option;
-        try {
-            do {
-                System.out.println("\t::Menu::");
-                System.out.println("1. Generar y guardar contraseña.");
-                System.out.println("2. Mostrar todas las contraseñas guardadas.");
-                System.out.println("3. Buscar una contraseña.");
-                System.out.print("4. Salir.");
-                System.out.print("Ingrese su opcion: ");
-                option = input.nextInt();
-                System.out.println();
-            }while (option!=4);
-        }catch (Exception e){
-            System.err.println("Error, debes de ingresar un numero...");
-            System.out.println();
-            menu();
         }
     }
 
@@ -59,5 +45,64 @@ public class Main {
                 "Desarrollado por @ComapactDevs");
         System.out.println();
         menu();
+        System.out.println();
+    }
+
+    public static void menu(){
+        int option;
+        try {
+            do {
+                System.out.println("\t::Menu::");
+                System.out.println("1. Generar y guardar contraseña.");
+                System.out.println("2. Mostrar todas las contraseñas guardadas.");
+                System.out.println("3. Buscar una contraseña.");
+                System.out.print("4. Salir.");
+                System.out.print("\nIngrese su opcion: ");
+                option = input.nextInt();
+                System.out.println();
+
+                switch (option){
+                    case 1:
+                        generateAndSavePassword();
+                        System.out.println();
+                        break;
+                    case 2:
+                        showAllPaswords();
+                        System.out.println();
+                        break;
+                    case 3:
+                        break;
+
+                    case 4:
+                        System.out.println("Saliendo del programa...");
+                        break;
+                    default:
+                        System.out.println("No existe esta opcion, intente nuevamente");
+                }
+            }while (option!=4);
+        }catch (Exception e){
+            System.err.println("Error, debes de ingresar un numero...");
+            System.out.println();
+            menu();
+        }
+    }
+
+    private static void generateAndSavePassword(){
+        input.nextLine();
+        String namePassword, password;
+        System.out.println("Generando una contraseña...");
+        GeneratePassoword generatePassoword = new GeneratePassoword();
+        password = generatePassoword.generatePassword();
+        System.out.println("La contraseña generada es: "+password);
+        System.out.print("Ahora ingrese un nombre como identificador para la contraseña: ");
+        namePassword = input.nextLine();
+        WriteFile writeFile = new WriteFile(writeBin, new ObtainingData(namePassword, password));
+        writeFile.writeInFile();
+        System.out.println("Datos guardardos correctamente.");
+    }
+
+    private static void showAllPaswords(){
+        ReadFile readFile = new ReadFile(readBin);
+        readFile.showPassowrds();
     }
 }
